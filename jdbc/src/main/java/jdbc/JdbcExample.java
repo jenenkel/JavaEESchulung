9a5cca3c2b6755792b61a82f147deddf8a057f2e
jkuttinal <jkuttinal@cofinpro.de>
2021-02-18 10:56:38 +0100

    Aufgabe 3

--- jdbc/src/main/java/jdbc/JdbcExample.java
+++ jdbc/src/main/java/jdbc/JdbcExample.java
@@ -14,21 +14,40 @@ public class JdbcExample {
 
             try (Statement statement = connection.createStatement()) {
                 // TODO Erzeuge Tabelle(n)
-                statement.execute("create table ...");
+                statement.execute("" +
+                    "create table Kleidung(id IDENTITY, " +
+                    "name varchar(100), " +
+                    "preis DOUBLE)");
+                statement.execute("" +
+                    "create table Kunde(id IDENTITY, " +
+                    "vorname varchar(50), " +
+                    "nachname varchar(50), " +
+                    "email varchar(100), " +
+                    "zipCode INT)");
+                statement.execute("create table Warenkorb(" +
+                    "id IDENTITY, " +
+                    "kunde_id BIGINT, " +
+                    "kleidungs_id BIGINT, " +
+                    "foreign key (kleidungs_id) references Kleidung(id), " +
+                    "foreign key (kunde_id) references Kunde(id))");
 
                 // TODO Befülle Tabelle(n)
-                statement.execute("insert into ...");
-                statement.execute("insert into ...");
-                statement.execute("insert into ...");
+                statement.execute("insert into Kleidung (name, preis) values('Hose', 20)");
+                statement.execute("insert into Kleidung (name, preis) values('Mütze', 15)");
+                statement.execute(
+                    "insert into Kunde (vorname, nachname, email, zipCode) values('Hans', 'Peter','a@b.de', 12451)");
+                statement.execute("insert into Warenkorb (kunde_id, kleidungs_id) values (1, 1)");
 
                 // TODO Stelle eine Anfrage an die Datenbank
-                try (ResultSet resultSet = statement.executeQuery("select ... from ... where ...")) {
+                try (ResultSet resultSet = statement.executeQuery("select * from Warenkorb w JOIN Kleidung k ON k.id = w.kleidungs_id")) {
                     // TODO Zeige Ergebnisse an
                     System.out.println("----------------------------------------");
                     while (resultSet.next()) {
-                        int someInt = resultSet.getInt("...column name...");
-                        String someString = resultSet.getString("...column name...");
+                        int someInt = resultSet.getInt("kunde_id");
+                        int someString = resultSet.getInt("kleidungs_id");
+                        String str = resultSet.getString("name");
                         System.out.println(someInt + " " + someString);
+                        System.out.println(str);
                     }
                     System.out.println("----------------------------------------");
                 }
